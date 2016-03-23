8159d1b83573a482b59473604ce5f6dc883f3a28
TK-999 <tk.999.wikia@gmail.com>
2016-03-23 21:13:09 +0100

    Clean up ActivityFeedTag.php

--- extensions/wikia/MyHome/ActivityFeedTag.php
+++ extensions/wikia/MyHome/ActivityFeedTag.php
@@ -1,58 +1,69 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
+$wgExtensionCredits['parserhook'][] = [
 	'path' => __FILE__,
 	'name' => 'ActivityFeedTag',
-	'author' => array('Inez Korczyński', '[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]'),
+	'author' => [ 'Inez Korczyński', '[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]' ],
 	'version' => '1.0',
 	'description' => 'Provides wiki activity data'
-);
+];
 
 $wgHooks['ParserFirstCallInit'][] = 'ActivityFeedTag_setup';
 
-function ActivityFeedTag_setup(Parser $parser) {
-	$parser->setHook('activityfeed', 'ActivityFeedTag_render');
+/**
+ * @param Parser $parser
+ * @return bool true
+ * @throws MWException
+ */
+function ActivityFeedTag_setup( Parser $parser ) {
+	$parser->setHook( 'activityfeed', 'ActivityFeedTag_render' );
 	return true;
 }
 
-function ActivityFeedTag_render($content, $attributes, $parser, $frame) {
-	global $wgEnableAchievementsInActivityFeed, $wgEnableAchievementsExt;
+/**
+ * @param string $content
+ * @param array $attributes
+ * @param Parser $parser
+ * @param PPFrame $frame
+ * @return string
+ * @throws WikiaException
+ */
+function ActivityFeedTag_render( $content, array $attributes, Parser $parser, PPFrame $frame ) {
+	$wg = F::app()->wg;
 
-	if (!class_exists('ActivityFeedHelper')) {
+	if ( !class_exists( 'ActivityFeedHelper' ) ) {
 		return '';
 	}
-	wfProfileIn(__METHOD__);
 
-	$parameters = ActivityFeedHelper::parseParameters($attributes);
+	$parameters = ActivityFeedHelper::parseParameters( $attributes );
 
-	$tagid = str_replace('.', '_', uniqid('activitytag_', true));	//jQuery might have a problem with . in ID
+	$tagid = str_replace( '.', '_', uniqid( 'activitytag_', true ) );    //jQuery might have a problem with . in ID
 	$jsParams = "size={$parameters['maxElements']}";
-	if (!empty($parameters['includeNamespaces'])) $jsParams .= "&ns={$parameters['includeNamespaces']}";
-	if (!empty($parameters['flags'])) $jsParams .= '&flags=' . implode('|', $parameters['flags']);
+	if ( !empty( $parameters['includeNamespaces'] ) ) $jsParams .= "&ns={$parameters['includeNamespaces']}";
+	if ( !empty( $parameters['flags'] ) ) $jsParams .= '&flags=' . implode( '|', $parameters['flags'] );
 	$parameters['tagid'] = $tagid;
 
-	$feedHTML = ActivityFeedHelper::getList($parameters);
+	$feedHTML = ActivityFeedHelper::getList( $parameters );
 
-	$style = empty($parameters['style']) ? '' : ' style="' . $parameters['style'] . '"';
+	$style = empty( $parameters['style'] ) ? '' : ' style="' . $parameters['style'] . '"';
 	$timestamp = wfTimestampNow();
 
-	$snippetsDependencies = array('/extensions/wikia/MyHome/ActivityFeedTag.js', '/extensions/wikia/MyHome/ActivityFeedTag.css');
+	$snippetsDependencies = [ '/extensions/wikia/MyHome/ActivityFeedTag.js', '/extensions/wikia/MyHome/ActivityFeedTag.css' ];
 
-	if((!empty($wgEnableAchievementsInActivityFeed)) && (!empty($wgEnableAchievementsExt))){
-		array_push($snippetsDependencies, '/extensions/wikia/AchievementsII/css/achievements_sidebar.css');
+	if ( $wg->EnableAchievementsInActivityFeed && $wg->EnableAchievementsExt ){
+		array_push( $snippetsDependencies, '/extensions/wikia/AchievementsII/css/achievements_sidebar.css' );
 	}
 
 	$snippets = JSSnippets::addToStack(
 		$snippetsDependencies,
 		null,
 		'ActivityFeedTag.initActivityTag',
-		array(
+		[
 			'tagid' => $tagid,
 			'jsParams' => $jsParams,
 			'timestamp' => $timestamp
-		)
+		]
 	);
 
-	wfProfileOut(__METHOD__);
 	return "<div$style>$feedHTML</div>$snippets";
 }
